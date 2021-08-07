5ba1ffe72feef9365a65dc24eaa543593be18634
Ivan Podvorniy <40368492+CoffeBee@users.noreply.github.com>
2021-08-07 20:11:46 +0300

    rename swift package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftNBT",
+    name: "swift-nbt",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
